7a87ce029a97a41161de2f00f245ab2d96408ef4
vansh__bhardwaj <39709733+vansh1999@users.noreply.github.com>
2023-06-23 09:59:32 +0530

    deployed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('added secret in webhook');
+  res.end('Hello Jenkins. We have automatically deployed !');
 });
 
 server.listen(port, hostname, () => {
